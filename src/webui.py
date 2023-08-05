ca48e6fc17a2568f2e4202659551bff336b174ba
SociallyIneptWeeb <86341112+SociallyIneptWeeb@users.noreply.github.com>
2023-08-05 17:34:39 +0800

    feat: Download from list of public voice models

--- src/webui.py
+++ src/webui.py
@@ -1,3 +1,4 @@
+import json
 import os
 import shutil
 import urllib.request
@@ -15,16 +16,26 @@ rvc_models_dir = os.path.join(BASE_DIR, 'rvc_models')
 output_dir = os.path.join(BASE_DIR, 'song_output')
 
 
-def get_models_list(models_dir):
+def get_current_models(models_dir):
     models_list = os.listdir(models_dir)
-    items_to_remove = ['hubert_base.pt', 'MODELS.txt']
-    models_clear_list = [item for item in models_list if item not in items_to_remove]
-    return models_clear_list
+    items_to_remove = ['hubert_base.pt', 'MODELS.txt', 'public_models.json']
+    return [item for item in models_list if item not in items_to_remove]
 
 
 def update_models_list():
-    models_l = get_models_list(rvc_models_dir)
+    models_l = get_current_models(rvc_models_dir)
     return gr.Dropdown.update(choices=models_l)
+
+
+def load_public_models():
+    models_table = []
+    for model in public_models['voice_models']:
+        if not model['name'] in voice_models:
+            model = [model['name'], model['description'], model['credit'], model['url'], ', '.join(model['tags'])]
+            models_table.append(model)
+
+    tags = list(public_models['tags'].keys())
+    return gr.DataFrame.update(value=models_table), gr.CheckboxGroup.update(choices=tags)
 
 
 def download_and_extract_zip(url, dir_name, progress=gr.Progress()):
@@ -71,17 +82,56 @@ def download_and_extract_zip(url, dir_name, progress=gr.Progress()):
         raise gr.Error(str(e))
 
 
+def filter_models(tags, query):
+    models_table = []
+
+    # no filter
+    if len(tags) == 0 and len(query) == 0:
+        for model in public_models['voice_models']:
+            models_table.append([model['name'], model['description'], model['credit'], model['url'], model['tags']])
+
+    # filter based on tags and query
+    elif len(tags) > 0 and len(query) > 0:
+        for model in public_models['voice_models']:
+            if all(tag in model['tags'] for tag in tags):
+                model_attributes = f"{model['name']} {model['description']} {model['credit']} {' '.join(model['tags'])}".lower()
+                if query.lower() in model_attributes:
+                    models_table.append([model['name'], model['description'], model['credit'], model['url'], model['tags']])
+
+    # filter based on only tags
+    elif len(tags) > 0:
+        for model in public_models['voice_models']:
+            if all(tag in model['tags'] for tag in tags):
+                models_table.append([model['name'], model['description'], model['credit'], model['url'], model['tags']])
+
+    # filter based on only query
+    else:
+        for model in public_models['voice_models']:
+            model_attributes = f"{model['name']} {model['description']} {model['credit']} {' '.join(model['tags'])}".lower()
+            if query.lower() in model_attributes:
+                models_table.append([model['name'], model['description'], model['credit'], model['url'], model['tags']])
+
+    return gr.DataFrame.update(value=models_table)
+
+
+def pub_dl_autofill(pub_models, event: gr.SelectData):
+    return gr.Text.update(value=pub_models.loc[event.index[0], 'URL']), gr.Text.update(value=pub_models.loc[event.index[0], 'Model Name'])
+
+
 if __name__ == '__main__':
     parser = ArgumentParser(description='Generate a AI cover song in the song_output/id directory.', add_help=True)
     parser.add_argument("--share", action="store_true", dest="share_enabled", default=False, help="Enable sharing")
     args = parser.parse_args()
     share_enabled = args.share_enabled
 
-    voice_models = get_models_list(rvc_models_dir)
+    voice_models = get_current_models(rvc_models_dir)
+    with open(os.path.join(rvc_models_dir, 'public_models.json')) as infile:
+        public_models = json.load(infile)
+
     with gr.Blocks(title='AICoverGenWebUI') as app:
-        
+
         gr.Label('AICoverGen WebUI created with ❤️', show_label=False)
-       
+
         # main tab
         with gr.Tab("Generate"):
             with gr.Row():
@@ -105,28 +155,57 @@ if __name__ == '__main__':
                 ref_btn.click(update_models_list, None, outputs=rvc_model)
                 is_webui = gr.Number(value=1, visible=False)
                 generate_btn.click(song_cover_pipeline, inputs=[song_input, rvc_model, pitch, keep_files, is_webui], outputs=[ai_cover])
-        
+
         # Download tab
         with gr.Tab("Download model"):
-            with gr.Row():
-                model_zip_link = gr.Text(label='Download link to model', info='Should be a zip file containing a .pth model file and an optional .index file.')
-                model_name = gr.Text(label='Name your model', info='Give your new model a unique name from your other voice models.')
 
-            with gr.Row():
-                download_btn = gr.Button('Download 🌐', variant='primary', show_progress=True, scale=19)
-                dl_output_message = gr.Text(label='Output Message', interactive=False, scale=20)
+            with gr.Tab('From HuggingFace URL'):
+                with gr.Row():
+                    model_zip_link = gr.Text(label='Download link to model', info='Should be a zip file containing a .pth model file and an optional .index file.')
+                    model_name = gr.Text(label='Name your model', info='Give your new model a unique name from your other voice models.')
 
-            download_btn.click(download_and_extract_zip, inputs=[model_zip_link, model_name], outputs=dl_output_message)
+                with gr.Row():
+                    download_btn = gr.Button('Download 🌐', variant='primary', scale=19)
+                    dl_output_message = gr.Text(label='Output Message', interactive=False, scale=20)
 
-            gr.Markdown('## Input Examples')
-            gr.Examples(
-                [
-                    ["https://huggingface.co/phant0m4r/LiSA/resolve/main/LiSA.zip", "Lisa"],
-                    ["https://huggingface.co/Kit-Lemonfoot/kitlemonfoot_rvc_models/resolve/main/AZKi%20(Hybrid).zip", "Azki"]
-                ],
-                [model_zip_link, model_name],
-                [],
-                download_and_extract_zip,
-            )
+                download_btn.click(download_and_extract_zip, inputs=[model_zip_link, model_name], outputs=dl_output_message)
+
+                gr.Markdown('## Input Examples')
+                gr.Examples(
+                    [
+                        ['https://huggingface.co/phant0m4r/LiSA/resolve/main/LiSA.zip', 'Lisa'],
+                        ['https://huggingface.co/Kit-Lemonfoot/kitlemonfoot_rvc_models/resolve/main/AZKi%20(Hybrid).zip', 'Azki']
+                    ],
+                    [model_zip_link, model_name],
+                    [],
+                    download_and_extract_zip,
+                )
+
+            with gr.Tab('From Public Index'):
+
+                gr.Markdown('## How to use')
+                gr.Markdown('- Click Initialize public models table')
+                gr.Markdown('- Filter models using tags or search bar')
+                gr.Markdown('- Select a row to autofill the download link and model name')
+                gr.Markdown('- Click Download')
+
+                with gr.Row():
+                    pub_zip_link = gr.Text(label='Download link to model')
+                    pub_model_name = gr.Text(label='Model name')
+
+                with gr.Row():
+                    download_pub_btn = gr.Button('Download 🌐', variant='primary', scale=19)
+                    pub_dl_output_message = gr.Text(label='Output Message', interactive=False, scale=20)
+
+                filter_tags = gr.CheckboxGroup(value=[], label='Show voice models with tags', choices=[])
+                search_query = gr.Text(label='Search')
+                load_public_models_button = gr.Button(value='Initialize public models table', variant='primary')
+
+                public_models_table = gr.DataFrame(value=[], headers=['Model Name', 'Description', 'Credit', 'URL', 'Tags'], label='Available Public Models', interactive=False)
+                public_models_table.select(pub_dl_autofill, inputs=[public_models_table], outputs=[pub_zip_link, pub_model_name])
+                load_public_models_button.click(load_public_models, outputs=[public_models_table, filter_tags])
+                search_query.change(filter_models, inputs=[filter_tags, search_query], outputs=public_models_table)
+                filter_tags.change(filter_models, inputs=[filter_tags, search_query], outputs=public_models_table)
+                download_pub_btn.click(download_and_extract_zip, inputs=[pub_zip_link, pub_model_name], outputs=pub_dl_output_message)
 
     app.launch(share=share_enabled, enable_queue=True)
